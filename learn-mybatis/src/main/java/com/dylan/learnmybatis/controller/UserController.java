992317049afc17e255d9481cf0c51ca6ad12f104
Dylan <1171357812@qq.com>
2021-04-14 17:06:00 +0800

    Mybatis Sample

---
+++ learn-mybatis/src/main/java/com/dylan/learnmybatis/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.dylan.learnmybatis.controller;
+
+import com.dylan.learnmybatis.model.dto.UserDto;
+import com.dylan.learnmybatis.model.vo.AimRes;
+import com.dylan.learnmybatis.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dylan
+ * @Date : Created in 14:04 2021/4/14
+ * @Description :
+ * @Function :
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("add")
+    public AimRes addUser(@RequestBody UserDto userDto){
+
+        return AimRes.success();
+    }
+
+    @RequestMapping("delete")
+    public AimRes deleteUser(@RequestBody UserDto userDto){
+
+        return AimRes.success();
+    }
+
+    @RequestMapping("update")
+    public AimRes updateUser(@RequestBody UserDto userDto){
+
+        return AimRes.success();
+    }
+
+    @RequestMapping("search")
+    @ResponseBody
+    public AimRes searchUser(){
+        return userService.getUsers();
+    }
+
+    @RequestMapping("getByName")
+    public AimRes searchUserByName(String userName){
+        return userService.getUserByName(userName);
+    }
+
+}
---
+++ learn-mybatis/src/main/java/com/dylan/learnmybatis/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.dylan.learnmybatis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dylan.learnmybatis.model.entity.UserEntity;
+import com.dylan.learnmybatis.model.vo.UserVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper extends BaseMapper<UserEntity> {
+
+    List<UserVo> getUsers();
+
+    UserVo getUserByName(String userName);
+
+}
