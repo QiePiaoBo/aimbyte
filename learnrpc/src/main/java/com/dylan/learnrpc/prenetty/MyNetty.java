bc9b155c2915b72699395c914df5046487cf74ec
Dylan <1171357812@qq.com>
2021-04-23 17:29:58 +0800

    不使用bootstrap实现一个客户端

--- learnrpc/src/main/java/com/dylan/learnrpc/prenetty/MyNetty.java
+++ learnrpc/src/main/java/com/dylan/learnrpc/prenetty/MyNetty.java
@@ -1,10 +1,14 @@
 package com.dylan.learnrpc.prenetty;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.buffer.*;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Dylan
@@ -16,14 +20,36 @@ public class MyNetty {
 
     public static void main(String[] args) {
 
-        myByteBuf();
-
+//        myByteBuf();
+        try {
+            clientModel();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static void clientModel(){
+    public static void clientModel() throws InterruptedException {
         NioEventLoopGroup thread = new NioEventLoopGroup(1);
 
+        NioSocketChannel client = new NioSocketChannel();
+
+        thread.register(client);
+
+        ChannelPipeline p = client.pipeline();
+        p.addLast(new MyHandler());
+
+        ChannelFuture connect = client.connect(new InetSocketAddress("192.168.105.53", 9090));
+        ChannelFuture sync = connect.sync();
+
+        ByteBuf byteBuf = Unpooled.copiedBuffer("Hello world".getBytes(StandardCharsets.UTF_8));
+        ChannelFuture send = client.writeAndFlush(byteBuf);
+        // 保证send动作完成
+        send.sync();
+
+        // 通过sync拿到channel后，确定断开后向下执行
+        sync.channel().closeFuture().sync();
+        System.out.println("client over...");
     }
 
     public static void myByteBuf(){
@@ -46,5 +72,30 @@ public class MyNetty {
         System.out.println("是否堆外：" + byteBuf.isDirect());
         System.out.println("实际大小：" + byteBuf.capacity());
         System.out.println("最大大小：" + byteBuf.maxCapacity());
+    }
+
+
+}
+class MyHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered.");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active.");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        CharSequence str = byteBuf.getCharSequence(0, byteBuf.readableBytes(), StandardCharsets.UTF_8);
+
+        ctx.writeAndFlush(byteBuf);
+        System.out.println(str);
+
+
     }
 }
