4599dcf9e232ba13abd18fde85a9d3668a4d9f60
Dylan <1171357812@qq.com>
2021-04-24 23:56:10 +0800

    手撸 Netty RPC 未完成版

--- learnrpc/src/main/java/com/dylan/learnrpc/prenetty/MyNetty.java
+++ learnrpc/src/main/java/com/dylan/learnrpc/prenetty/MyNetty.java
@@ -1,13 +1,16 @@
 package com.dylan.learnrpc.prenetty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.*;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.util.concurrent.EventExecutorGroup;
 
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -22,13 +25,24 @@ public class MyNetty {
 
 //        myByteBuf();
         try {
-            clientModel();
+//            客户端模式
+//            clientModel();
+//            服务端模式
+//            serverMode();
+//            netty客户端模式
+//            nettyClient();
+//            Netty服务端模式
+            nettyServer();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
     }
 
+    /**
+     * 客户端模式
+     * @throws InterruptedException
+     */
     public static void clientModel() throws InterruptedException {
         NioEventLoopGroup thread = new NioEventLoopGroup(1);
 
@@ -37,7 +51,7 @@ public class MyNetty {
         thread.register(client);
 
         ChannelPipeline p = client.pipeline();
-        p.addLast(new MyHandler());
+        p.addLast(new MyClientHandler());
 
         ChannelFuture connect = client.connect(new InetSocketAddress("192.168.105.53", 9090));
         ChannelFuture sync = connect.sync();
@@ -52,31 +66,175 @@ public class MyNetty {
         System.out.println("client over...");
     }
 
-    public static void myByteBuf(){
-        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer(8, 20);
-        ByteBuf byteBuf1 = UnpooledByteBufAllocator.DEFAULT.buffer(8, 20);
-        ByteBuf byteBuf2 = PooledByteBufAllocator.DEFAULT.buffer(8, 20);
+    /**
+     * 服务端模式
+     * @throws InterruptedException
+     */
+    public static void serverMode() throws InterruptedException {
 
+        NioEventLoopGroup thread = new NioEventLoopGroup(1);
 
-        print(byteBuf);
-        byteBuf.writeBytes(new byte[]{1, 2, 3, 4});
-        print(byteBuf);
-    }
-    public static void print(ByteBuf byteBuf){
-        System.out.println("是否可读：" + byteBuf.isReadable());
-        System.out.println("可读位置：" + byteBuf.readerIndex());
-        System.out.println("可读大小：" + byteBuf.readableBytes());
-        System.out.println("是否可写：" + byteBuf.isWritable());
-        System.out.println("可写位置：" + byteBuf.writerIndex());
-        System.out.println("可写大小：" + byteBuf.writableBytes());
-        System.out.println("是否堆外：" + byteBuf.isDirect());
-        System.out.println("实际大小：" + byteBuf.capacity());
-        System.out.println("最大大小：" + byteBuf.maxCapacity());
+        NioServerSocketChannel server = new NioServerSocketChannel();
+        // 注册server
+        thread.register(server);
+        ChannelPipeline p = server.pipeline();
+        // 在server的pipeline中添加接收连接的handler
+        p.addLast(new MyAcceptHandler(thread, new ChannelInitHandler()));
+        // 绑定端口，准备好接收连接
+        ChannelFuture bind = server.bind(new InetSocketAddress("192.168.0.110", 9090));
+        bind.sync().channel().closeFuture().sync();
+        System.out.println("server closed.");
     }
 
+    /**
+     * Netty的客户端
+     * @throws InterruptedException
+     */
+    public static void nettyClient() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+        Bootstrap bs = new Bootstrap();
+        ChannelFuture connect = bs.group(group)
+                .channel(NioSocketChannel.class)
+//                .handler(new ChannelInitHandler())
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline p = socketChannel.pipeline();
+                        p.addLast(new MyClientHandler());
+                    }
+                })
+                .connect(new InetSocketAddress("192.168.110.110", 9090));
+        // 连接结束（sync()）后,先获取到连接的channel
+        Channel client = connect.sync().channel();
 
+        ByteBuf byteBuf = Unpooled.copiedBuffer("Hello server".getBytes());
+        ChannelFuture send = client.writeAndFlush(byteBuf);
+
+        send.sync();
+
+        client.closeFuture().sync();
+
+
+    }
+
+    /**
+     * Netty的服务端
+     */
+    public static void nettyServer() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+        ServerBootstrap sbs = new ServerBootstrap();
+
+        ChannelFuture bind = sbs.group(group, group)
+                .channel(NioServerSocketChannel.class)
+//                .handler(new MyAcceptHandler(group, new ChannelInitHandler()))
+//                .childHandler(new ChannelInitHandler())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new MyClientHandler());
+                    }
+                })
+                .bind(new InetSocketAddress("192.168.0.110", 9090));
+        Channel channel = bind.sync().channel();
+
+        channel.closeFuture().sync();
+    }
+
+
+//    检测新建的、写入一次的、写入两次的、写入三次的、写满之后的ByteBuf的状态
+//    public static void myByteBuf(){
+//        ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer(8, 20);
+//        ByteBuf byteBuf1 = UnpooledByteBufAllocator.DEFAULT.buffer(8, 20);
+//        ByteBuf byteBuf2 = PooledByteBufAllocator.DEFAULT.buffer(8, 20);
+//
+//
+//        print(byteBuf);
+//        byteBuf.writeBytes(new byte[]{1, 2, 3, 4});
+//        print(byteBuf);
+//    }
+//    public static void print(ByteBuf byteBuf){
+//        System.out.println("是否可读：" + byteBuf.isReadable());
+//        System.out.println("可读位置：" + byteBuf.readerIndex());
+//        System.out.println("可读大小：" + byteBuf.readableBytes());
+//        System.out.println("是否可写：" + byteBuf.isWritable());
+//        System.out.println("可写位置：" + byteBuf.writerIndex());
+//        System.out.println("可写大小：" + byteBuf.writableBytes());
+//        System.out.println("是否堆外：" + byteBuf.isDirect());
+//        System.out.println("实际大小：" + byteBuf.capacity());
+//        System.out.println("最大大小：" + byteBuf.maxCapacity());
+//    }
 }
-class MyHandler extends ChannelInboundHandlerAdapter {
+
+/**
+ * 服务端接收连接的handler
+ */
+class MyAcceptHandler extends ChannelInboundHandlerAdapter {
+
+    private final EventLoopGroup selector;
+    private final ChannelHandler channelInitHandler;
+
+    /**
+     * 初始化该类时，传入注册用的selector和桥梁handler，桥梁handler的作用是作为一个通用的handler向客户端添加其他自定义的handler，添加完毕后会将自己从客户端的pipeline中删掉
+     * @param selector
+     * @param channelInitHandler
+     */
+    public MyAcceptHandler(EventLoopGroup selector, ChannelInitHandler channelInitHandler) {
+        this.selector = selector;
+        this.channelInitHandler = channelInitHandler;
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered.");
+    }
+
+    /**
+     * 这是server的用于接收的handler，所以这个方法一定是框架帮我们处理好的一个连接
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        // 转换成一个客户端连接
+        SocketChannel client = (SocketChannel) msg;
+        // 在客户端连接的pipeLine中添加桥梁handler
+        client.pipeline().addLast(channelInitHandler);
+        // 将客户端连接注册到服务端连接所在的selector
+        selector.register(client);
+    }
+}
+
+/**
+ * 桥梁handler
+ * 将这个方法作为桥梁,它也是一个handler，是shareable的，它的作用是在客户端注册成功后把一些用户想要用到的自定义handler添加到pipeline中去，然后再将自己从pipeline中剔除
+ * 这样不会导致同一个handler对象的重复利用
+ */
+@ChannelHandler.Sharable
+class ChannelInitHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当注册成功后，将一些自定义的handler给注册到客户端的pipeline，最后功成身退，将自己从客户端的pipeline中remove掉
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        Channel client = ctx.channel();
+        ChannelPipeline p = client.pipeline();
+        p.addLast(new MyClientHandler());
+        p.remove(this);
+    }
+}
+/**
+ * 用户自定义handler
+ * 这个handler应该是用户自己来实现的，不应该强行复用到pipeline中，但是可以用一个公共的桥梁将自己添加
+ */
+//@ChannelHandler.Sharable
+class MyClientHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
@@ -99,3 +257,4 @@ class MyHandler extends ChannelInboundHandlerAdapter {
 
     }
 }
+
---
+++ learnrpc/src/main/java/com/dylan/learnrpc/prenetty/MyRPCTest.java
@@ -0,0 +1,395 @@
+package com.dylan.learnrpc.prenetty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.InetSocketAddress;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Dylan
+ * @Date : 2021/4/24 - 13:34
+ * @Description : 需求：通信、连接数量、拆包、动态代理、序列化反序列化、协议封装、连接池
+ * @Function :
+ */
+public class MyRPCTest {
+
+    public static void main(String[] args) {
+//        模拟客户端
+        get();
+    }
+
+    public static void get(){
+        new Thread(()->{
+            server();
+        }).start();
+        System.out.println("Server started.");
+        Car car = proxyGet(Car.class); // 动态代理
+        car.ooxx("hello");
+
+
+//        Fly fly = proxyGet(Fly.class); // 动态代理
+//        fly.xxoo("hello");
+    }
+
+    public static void server(){
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = boss;
+        ServerBootstrap sbs = new ServerBootstrap();
+        ChannelFuture bind = sbs.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        System.out.println("server accept client port : " + nioSocketChannel.remoteAddress().getPort());
+                        ChannelPipeline p = nioSocketChannel.pipeline();
+                        p.addLast(new ServerRequestHandler());
+                    }
+                }).bind(new InetSocketAddress("localhost", 9090));
+        try {
+            bind.sync().channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 根据入参获取对象
+     * @param interfaceInfo
+     * @param <T>
+     * @return
+     */
+    public static <T>T proxyGet(Class<T> interfaceInfo){
+        // 实现各个版本的动态代理
+        ClassLoader classLoader = interfaceInfo.getClassLoader();
+        Class<?>[] methodInfo = {interfaceInfo};
+
+
+        return (T)Proxy.newProxyInstance(classLoader, methodInfo, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+                // 需要在这里处理客户端对服务方的调用过程
+                // 1. 调用服务、方法、参数  -> 封装成message     [content]
+                String name = interfaceInfo.getName();
+                String methodName = method.getName();
+                Class<?>[] parameterTypes = method.getParameterTypes();
+
+                MyContent myContent = new MyContent();
+                myContent.setArgs(args);
+                myContent.setMethodName(methodName);
+                myContent.setName(name);
+                myContent.setParameterTypes(parameterTypes);
+
+                ByteArrayOutputStream outInMemory = new ByteArrayOutputStream();
+                ObjectOutputStream out = new ObjectOutputStream(outInMemory);
+                out.writeObject(myContent);
+                byte[] msgBody = outInMemory.toByteArray();
+                // 2. requestID + message , 本地要缓存这些信息
+                // 协议：【header】【msgBody】
+                MyHeader header = createHeader(msgBody);
+                outInMemory.reset();
+                out = new ObjectOutputStream(outInMemory);
+                out.writeObject(header);
+                byte[] msgHeader = outInMemory.toByteArray();
+                // 3. 连接池，用于取得与服务端的连接
+                ClientFactory factory = ClientFactory.getFactory();
+                NioSocketChannel clientChannel = factory.getClient(new InetSocketAddress("localhost", 9090));
+                // 4. 发送 -->通过IO,将相关的调用信息发出去
+                CountDownLatch countDownLatch = new CountDownLatch(1);
+                long requestId = header.getRequestId();
+                ResponseHandler.addCallBack(requestId, new Runnable() {
+                    @Override
+                    public void run() {
+                        countDownLatch.countDown();
+                    }
+                });
+                ByteBuf byteBuf = PooledByteBufAllocator.DEFAULT.directBuffer(msgHeader.length + msgBody.length);
+                byteBuf.writeBytes(msgHeader);
+                byteBuf.writeBytes(msgBody);
+                ChannelFuture channelFuture = clientChannel.writeAndFlush(byteBuf);
+                channelFuture.sync();
+                // 阻塞住，直至有事件发生
+                countDownLatch.await();
+
+                // 5. 过了一段时间后，返回从对面回来了，怎么将代码再次回到这里并往下执行（睡眠、回调等方法，需要让线程停下来，而且结果回来的时候还能再继续走下去）
+
+                return null;
+            }
+        });
+    }
+
+    public static MyHeader createHeader(byte[] msgBody){
+        MyHeader header = new MyHeader();
+        int size = msgBody.length;
+        int f = 0x14141414;
+        long requestId = Math.abs(UUID.randomUUID().getLeastSignificantBits());
+        header.setFlag(f);
+        header.setDataLen(size);
+        header.setRequestId(requestId);
+
+
+        return header;
+    }
+}
+
+class ServerRequestHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        ByteBuf sendBuf = buf.copy();
+
+        System.out.println("channel start: " + buf.readableBytes());
+        if (buf.readableBytes() >= 110){
+            byte[] bytes = new byte[110];
+            buf.readBytes(bytes);
+            ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+            ObjectInputStream oin = new ObjectInputStream(in);
+            MyHeader header = (MyHeader) oin.readObject();
+            System.out.println("Server data length: " + header.dataLen);
+            System.out.println("Server requestId: " + header.getRequestId());
+
+            if (buf.readableBytes() >= header.getDataLen()){
+                byte[] data = new byte[(int)header.getDataLen()];
+                buf.readBytes(data);
+                ByteArrayInputStream din = new ByteArrayInputStream(data);
+                ObjectInputStream doin = new ObjectInputStream(din);
+                MyContent content = (MyContent) doin.readObject();
+
+                System.out.println("Server get requestBody.name: " + content.getName());
+            }else {
+                System.out.println("channel else: " + buf.readableBytes());
+            }
+        }
+        ChannelFuture channelFuture = ctx.writeAndFlush(sendBuf);
+        channelFuture.sync();
+    }
+}
+
+
+
+class ClientFactory{
+    NioEventLoopGroup clientWorker;
+    int poolSize = 1;
+    Random rand = new Random();
+    private ClientFactory(){
+    }
+    private static final ClientFactory factory;
+    static {
+        factory = new ClientFactory();
+    }
+    public static ClientFactory getFactory(){
+        return factory;
+    }
+
+    // 一个客户端可以连接很多个服务端，每个服务端都有自己的连接池
+    ConcurrentHashMap<InetSocketAddress, ClientPool> outBox = new ConcurrentHashMap<>();
+
+    public NioSocketChannel getClient(InetSocketAddress address){
+
+        ClientPool clientPool = outBox.get(address);
+        if (clientPool == null){
+            outBox.putIfAbsent(address, new ClientPool(poolSize));
+            clientPool = outBox.get(address);
+        }
+        // 现在得到的池是空的,随机取得一个下标，去取连接
+        int i = rand.nextInt(poolSize);
+        if (clientPool.clients[i] != null && clientPool.clients[i].isActive()){
+            return clientPool.clients[i];
+        }
+        synchronized (clientPool.lock[i]){
+            clientPool.clients[i] = create(address);
+            return clientPool.clients[i];
+        }
+    }
+
+    private NioSocketChannel create(InetSocketAddress address) {
+        // 基于netty的客户端
+        clientWorker = new NioEventLoopGroup(1);
+        Bootstrap bs = new Bootstrap();
+        ChannelFuture connect = bs.group(clientWorker)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        ChannelPipeline pipeline = nioSocketChannel.pipeline();
+                        pipeline.addLast(new ClientResponses());
+                    }
+                }).connect(address);
+        NioSocketChannel client = null;
+        try {
+            client = (NioSocketChannel) connect.sync().channel();
+            return client;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
+class ResponseHandler{
+    private static ConcurrentHashMap<Long, Runnable> mapping = new ConcurrentHashMap<>();
+
+    public static void addCallBack(long requestId, Runnable cb){
+        mapping.putIfAbsent(requestId, cb);
+    }
+
+    public static void runCallBack(long requestId){
+
+        Runnable runnable = mapping.get(requestId);
+        runnable.run();
+        removeCB(requestId);
+    }
+
+    private static void removeCB(long requestId){
+        mapping.remove(requestId);
+    }
+
+}
+
+
+
+class ClientResponses extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        if (buf.readableBytes() >= 110){
+            byte[] bytes = new byte[110];
+            buf.readBytes(bytes);
+            ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+            ObjectInputStream oin = new ObjectInputStream(in);
+            MyHeader header = (MyHeader) oin.readObject();
+            System.out.println("client datalength: " + header.dataLen);
+            System.out.println("client requestId: " + header.getRequestId());
+            //
+            ResponseHandler.runCallBack(header.requestId);
+//            if (buf.readableBytes() >= header.getDataLen()){
+//                byte[] data = new byte[(int)header.getDataLen()];
+//                buf.readBytes(data);
+//                ByteArrayInputStream din = new ByteArrayInputStream(data);
+//                ObjectInputStream doin = new ObjectInputStream(din);
+//                MyContent content = (MyContent) doin.readObject();
+//
+//                System.out.println(content.getName());
+//            }
+        }
+
+    }
+}
+
+
+class ClientPool{
+    NioSocketChannel[] clients;
+    Object[] lock;
+
+    ClientPool(int size){
+        clients = new NioSocketChannel[size]; // init 连接都是空的
+        lock = new Object[size]; // 锁是可以初始化的
+        for (int i = 0; i < size; i++) {
+            lock[i] = new Object();
+        }
+    }
+
+
+}
+
+class MyHeader implements Serializable{
+    // 通信上的协议
+    // 1. 标识，协议类型
+    // 2. UUID，requestId
+    // 3. DATA_LEN
+    int flag; // 32bit
+    long requestId;
+    long dataLen;
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public long getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(long requestId) {
+        this.requestId = requestId;
+    }
+
+    public long getDataLen() {
+        return dataLen;
+    }
+
+    public void setDataLen(long dataLen) {
+        this.dataLen = dataLen;
+    }
+}
+
+class MyContent implements Serializable{
+    private String name;
+    private String methodName;
+    private Class<?>[] parameterTypes;
+    private Object[] args;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Class<?>[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class<?>[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+}
+
+interface Car{
+    public void ooxx(String msg);
+
+}
+
+interface Fly{
+    void xxoo(String msg);
+}
