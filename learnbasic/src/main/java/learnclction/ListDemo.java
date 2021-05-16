9cb46514e290b4ffcd0ea8653e8e70af14e38d62
Dylan <1171357812@qq.com>
2021-05-16 21:58:26 +0800

    ListDemo

--- learnbasic/src/main/java/learnclction/ListDemo.java
+++ learnbasic/src/main/java/learnclction/ListDemo.java
@@ -1,6 +1,7 @@
 package learnclction;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,7 +12,28 @@ import java.util.List;
  */
 public class ListDemo {
     public static void main(String[] args) {
-        List list = new ArrayList<Integer>();
+        List list = new ArrayList();
+
+        list.add(1);
+        list.add(4);
+        list.add(2);
+        list.add(3);
+        list.add("asd");
+
+        list.remove(2);
+        for (Object o : list){
+            System.out.println(o);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        Iterator it = list.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
 
 
     }
