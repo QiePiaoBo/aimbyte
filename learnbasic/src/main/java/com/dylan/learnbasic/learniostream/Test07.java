09dd16834ed6a47d46d77189cbe1e9e7748bfc48
Dylan <1171357812@qq.com>
2021-05-24 17:35:03 +0800

    ioStream复制非文本文件

--- learnbasic/src/main/java/com/dylan/learnbasic/learniostream/Test07.java
+++ learnbasic/src/main/java/com/dylan/learnbasic/learniostream/Test07.java
@@ -2,6 +2,7 @@ package com.dylan.learnbasic.learniostream;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  * @author Dylan
@@ -15,15 +16,18 @@ public class Test07 {
         // 使用字节流将文件的内容读取到程序中来
         // 有一个源文件
         File source = new File("fileTestDir/test04/source.txt");
+        File testOut = new File("fileTestDir/test04/out.txt");
         // 准备一个字节流作为管道怼到源文件上
         FileInputStream fis = new FileInputStream(source);
+        FileOutputStream fot = new FileOutputStream(testOut);
         // 开始读取动作
-        int n = fis.read();
-        while (n != -1){
+        int n;
+        while ((n = fis.read()) != -1){
             System.out.println(n);
-            n = fis.read();
+            fot.write(n);
         }
         // 关闭流
+        fot.close();
         fis.close();
     }
 }
---
+++ learnbasic/src/main/java/com/dylan/learnbasic/learniostream/Test08.java
@@ -0,0 +1,36 @@
+package com.dylan.learnbasic.learniostream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * @author Dylan
+ * @Date : Created in 13:03 2021/5/24
+ * @Description :
+ * @Function :
+ */
+public class Test08 {
+    public static void main(String[] args) throws Exception {
+        // FileInputStream + FileOutputStream 图片复制
+//        File sourceImg = new File("fileTestDir/test04/source.jpg");
+//        File aimImg = new File("fileTestDir/test04/aim.jpg");
+//
+//        FileInputStream fin  = new FileInputStream(sourceImg);
+//        FileOutputStream fot = new FileOutputStream(aimImg);
+//
+//        int n;
+//        byte[] buffer = new byte[1024];
+//        while (fin.read(buffer) != -1){
+//            fot.write(buffer);
+//
+//        }
+//
+//        fot.close();
+//        fin.close();
+        File file = new File("fileTestDir/test050");
+        file.deleteOnExit();
+
+    }
+}
